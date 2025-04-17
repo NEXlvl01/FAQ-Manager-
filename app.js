9a614065a52ea38a5169bd83a0ee8f6cc3f910dc
NEXlvl01 <armanthakur2004@gmail.com>
2025-04-17 16:37:16 +0530

    Database Connection Has Been Done

--- app.js
+++ app.js
@@ -3,26 +3,23 @@ const cors = require('cors');
 const connectDB = require('./config/db');
 const dotenv = require('dotenv');
 
-// Load env vars
 dotenv.config();
 
-// Connect to database
 connectDB();
 
 const app = express();
 
-// Body parser
+
 app.use(express.json());
 
-// Enable CORS
+
 app.use(cors());
 
-// Mount routes
+
 app.use('/api/faqs', require('./routes/faqRoutes'));
 
-// Basic route
+
 app.get('/', (req, res) => {
-  res.send('FAQ Manager API is running...');
-});
+  res.send('FAQ Manager API is running...'
 
 module.exports = app;
